94f7d2d6d45df6efd4155f9845f004fb42a20a9f
Siim Puustusmaa <Siim.Puustusmaa@smit.ee>
2019-11-12 01:31:17 +0200

    simple sql queries can easily implemented via jpa queries

--- src/main/java/Library/infrastructure/BookRepository.java
+++ src/main/java/Library/infrastructure/BookRepository.java
@@ -1,10 +1,13 @@
 package Library.infrastructure;
 
+import java.util.List;
+
 import Library.model.Book;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-@Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+    // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+    List<Book> findAllByTitleContainingOrAuthorContainingAllIgnoreCase(
+            String titleSearchString, String authorSearchString);
 }
